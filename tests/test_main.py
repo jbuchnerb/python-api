3a01e8e4d97a68c3f66655b54c940176bbb26e0a
Johann Büchner <jbuchner.b@gmail.com>
2025-07-09 01:10:11 -0400

    ajuste 4

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,10 @@
 import sys
 import os
-
-
-# Agrega la carpeta raíz del proyecto al sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 from fastapi.testclient import TestClient
 from app.main import app
 
+# Agrega la carpeta raíz del proyecto al sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 client = TestClient(app)
 
