5a9cc71f752f85095b586c76c5e2d6ad3201b4ca
Johann Büchner <jbuchner.b@gmail.com>
2025-07-09 01:07:11 -0400

    ajuste 3

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,23 @@
 import sys
 import os
 
+
 # Agrega la carpeta raíz del proyecto al sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 from fastapi.testclient import TestClient
 from app.main import app
 
+
 client = TestClient(app)
+
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hola, mundo"}
+
 
 def test_saludo():
     response = client.get("/saludo/Johann")
